29461a60a3608963d1166e450f4bcdd58397203e
Yang Song <yang.song.te@renesas.com>
2018-02-20 16:35:07 +0800

    Add approx poly DP

--- OpenCVDemo/Detect2D.cpp
+++ OpenCVDemo/Detect2D.cpp
@@ -15,8 +15,8 @@ static Point lowLeft, highLeft, highRight, lowRight, lowRightCenter;
 static void detectMarker(void);
 static void getBarcode2D(Mat &input, Mat &output);
 static void getBarcode2DCenter(Mat &input, Mat &output);
-static void drawQuadrilateral(vector<Point> quadrilateral, Scalar color);
-static void drawConvexHull(int index, vector<int> hull, Scalar color);
+static void drawQuadrilateral(vector<Point> &quadrilateral, Scalar color);
+static void drawConvexHull(vector<Point> &hull, Scalar color);
 static void printArea(void);
 
 void detect2D(Mat &input, Mat &output, Mat &draw, int scale)
@@ -60,11 +60,11 @@ static void getSortedArea(void)
 	}
 }
 
-static vector<Point> getMinRect(int index)
+static vector<Point> getMinRect(vector<Point> &contour)
 {
 	vector<Point> minRect;
 	Point2f temp[4];
-	minAreaRect(Mat(contours[index])).points(temp);
+	minAreaRect(Mat(contour)).points(temp);
 	for (int i = 0; i < 4; i++) {
 		minRect.push_back(Point(temp[i].x, temp[i].y));
 	}
@@ -81,6 +81,17 @@ static vector<Point> getMinRectMid(vector<Point> &minRect)
 	return minRectMid;
 }
 
+static vector<Point> getConvexHull(vector<Point> &contour)
+{
+	vector<Point> originHull;
+	vector<int> hull;
+	convexHull(Mat(contour), hull, false); // false is clockwise, why?
+	for (int i = 0; i < hull.size(); i++) {
+		originHull.push_back(contour[hull[i]]);
+	}
+	return originHull;
+}
+
 static int len2(Point &p1, Point &p2)
 {
 	int x = p1.x - p2.x;
@@ -93,21 +104,21 @@ static int dot(Point &p1, Point &p2, Point &p3, Point &p4)
 	return (p2.x - p1.x) * (p4.x - p3.x) + (p2.y - p1.y) * (p4.y - p3.y);
 }
 
-static vector<int> reduceConvexHull(int index, vector<int> &originHull)
+static vector<Point> getReduceConvexHull(vector<Point> &originHull)
 {
-	vector<int> reduceHull;
+	vector<Point> reduceHull;
 	int size = originHull.size();
-	Point prev = contours[index][originHull[size - 1]];
-	Point cur = contours[index][originHull[0]];
+	Point prev = originHull[size - 1];
+	Point cur = originHull[0];
 	Point next;
 	float lenPrev = len2(prev, cur);
 	float lenCur;
 	for (int i = 0; i < originHull.size(); i++) {
-		next = contours[index][originHull[(i + 1) % size]];
+		next = originHull[(i + 1) % size];
 		lenCur = len2(cur, next);
 		int _dot = dot(cur, prev, cur, next);
 		if (_dot * _dot < lenPrev * lenCur * COS_160 * COS_160) {
-			reduceHull.push_back(originHull[i]);
+			reduceHull.push_back(cur);
 		}
 		prev = cur;
 		cur = next;
@@ -116,10 +127,10 @@ static vector<int> reduceConvexHull(int index, vector<int> &originHull)
 	return reduceHull;
 }
 
-static bool validateEdge(int index, vector<int> &hull, int hullIndex, vector<Point> &mid, int midIndex, int det)
+static bool validateEdge(vector<Point> &hull, int hullIndex, vector<Point> &mid, int midIndex, int det)
 {
-	Point p1 = contours[index][hull[hullIndex]];
-	Point p2 = contours[index][hull[(hullIndex + 1) % hull.size()]];
+	Point p1 = hull[hullIndex];
+	Point p2 = hull[(hullIndex + 1) % hull.size()];
 	Point m1 = mid[midIndex];
 	Point m2 = mid[midIndex + 2];
 	int sign1, sign2;
@@ -153,46 +164,111 @@ static Point getIntersection(vector<int> &e1, vector<int> &e2)
 	return Point(x, y);
 }
 
-static vector<Point> getMinQuadrilateral(int index)
+static vector<Point> getMinQuadrilateral(vector<Point> &contour)
 {
 	vector<Point> minQuadrilateral;
-	vector<int> originHull;
 	vector<int> validEdge;
 	int i;
 	int activeMid;
-	convexHull(Mat(contours[index]), originHull, false); // false is clockwise, why?
-	vector<int> &reduceHull = reduceConvexHull(index, originHull);
-	//vector<int> &reduceHull = originHull;
+
+	/*vector<Point> approx;
+	approxPolyDP(Mat(contours[index]), approx, arcLength(Mat(contours[index]), true)*0.03, true);
+	if (approx.size() == 4) {
+		drawQuadrilateral(approx, BLUE);
+		return approx;
+	}*/
+
+	vector<Point> &originHull = getConvexHull(contour);
+	vector<Point> &reduceHull = getReduceConvexHull(originHull);
+	//vector<Point> &reduceHull = originHull;
 	if (reduceHull.size() < 4) return minQuadrilateral;
-	//drawConvexHull(index, reduceHull, BLUE); //
+	//drawConvexHull(reduceHull, BLUE); //
 	int size = reduceHull.size();
-	vector<Point> &minRectMid = getMinRectMid(getMinRect(index));
+	vector<Point> &minRectMid = getMinRectMid(getMinRect(contour));
 	int det[2] = {
 		minRectMid[0].x * minRectMid[2].y - minRectMid[2].x * minRectMid[0].y,
 		minRectMid[1].x * minRectMid[3].y - minRectMid[3].x * minRectMid[1].y
 	};
 	for (i = 0; i < size; i++) {
-		if (validateEdge(index, reduceHull, i, minRectMid, 0, det[0])) {
+		if (validateEdge(reduceHull, i, minRectMid, 0, det[0])) {
 			validEdge.push_back(i);
 			activeMid = 1;
 			break;
 		}
-		else if (validateEdge(index, reduceHull, i, minRectMid, 1, det[1])) {
+		else if (validateEdge(reduceHull, i, minRectMid, 1, det[1])) {
 			validEdge.push_back(i);
 			activeMid = 0;
 			break;
 		}
 	}
 	for (i++; i < size; i++) {
-		if (validateEdge(index, reduceHull, i, minRectMid, activeMid, det[activeMid])) {
+		if (validateEdge(reduceHull, i, minRectMid, activeMid, det[activeMid])) {
 			validEdge.push_back(i);
 			activeMid = 1 - activeMid;
 		}
 	}
 	if (validEdge.size() != 4) return minQuadrilateral;
 	for (i = 0; i < 4; i++) {
-		vector<int> &e1 = getLineEquation(contours[index][reduceHull[validEdge[i]]], contours[index][reduceHull[(validEdge[i] + 1) % size]]);
-		vector<int> &e2 = getLineEquation(contours[index][reduceHull[validEdge[(i + 1) % 4]]], contours[index][reduceHull[(validEdge[(i + 1) % 4] + 1) % size]]);
+		vector<int> &e1 = getLineEquation(reduceHull[validEdge[i]], reduceHull[(validEdge[i] + 1) % size]);
+		vector<int> &e2 = getLineEquation(reduceHull[validEdge[(i + 1) % 4]], reduceHull[(validEdge[(i + 1) % 4] + 1) % size]);
+		Point temp = getIntersection(e1, e2);
+		if (temp.x < 0) return minQuadrilateral;
+		minQuadrilateral.push_back(temp);
+	}
+	return minQuadrilateral;
+}
+
+static vector<Point> getReverseOrder(vector<Point> &input)
+{
+	vector<Point> output;
+	int size = input.size();
+	for (int i = 0; i < size; i++) {
+		output.push_back(input[size - 1 - i]);
+	}
+	return output;
+}
+
+static vector<Point> getMinQuadrilateralDP(vector<Point> &contour)
+{
+	vector<Point> minQuadrilateral;
+	vector<int> validEdge;
+	int i;
+	int activeMid;
+
+	vector<Point> approx;
+	approxPolyDP(Mat(contour), approx, arcLength(Mat(contour), true)*0.02, true);
+	if (approx.size() > 5) return minQuadrilateral;
+	vector<Point> &reduceHull = getReverseOrder(approx);
+	if (reduceHull.size() < 4) return minQuadrilateral;
+	//drawConvexHull(reduceHull, BLUE); //
+	int size = reduceHull.size();
+	vector<Point> &minRectMid = getMinRectMid(getMinRect(contour));
+	int det[2] = {
+		minRectMid[0].x * minRectMid[2].y - minRectMid[2].x * minRectMid[0].y,
+		minRectMid[1].x * minRectMid[3].y - minRectMid[3].x * minRectMid[1].y
+	};
+	for (i = 0; i < size; i++) {
+		if (validateEdge(reduceHull, i, minRectMid, 0, det[0])) {
+			validEdge.push_back(i);
+			activeMid = 1;
+			break;
+		}
+		else if (validateEdge(reduceHull, i, minRectMid, 1, det[1])) {
+			validEdge.push_back(i);
+			activeMid = 0;
+			break;
+		}
+	}
+	for (i++; i < size; i++) {
+		if (validateEdge(reduceHull, i, minRectMid, activeMid, det[activeMid])) {
+			validEdge.push_back(i);
+			activeMid = 1 - activeMid;
+		}
+	}
+	if (validEdge.size() != 4) return minQuadrilateral;
+	for (i = 0; i < 4; i++) {
+		vector<int> &e1 = getLineEquation(reduceHull[validEdge[i]], reduceHull[(validEdge[i] + 1) % size]);
+		vector<int> &e2 = getLineEquation(reduceHull[validEdge[(i + 1) % 4]], reduceHull[(validEdge[(i + 1) % 4] + 1) % size]);
 		Point temp = getIntersection(e1, e2);
 		if (temp.x < 0) return minQuadrilateral;
 		minQuadrilateral.push_back(temp);
@@ -302,7 +378,7 @@ static void detectMarker(void)
 	marker.clear();
 	getSortedArea();
 	for (int i = 0; i < sortedIndex.size(); i++) {
-		vector<Point> &temp = getMinQuadrilateral(sortedIndex[i]);
+		vector<Point> &temp = getMinQuadrilateral(contours[sortedIndex[i]]);
 		if (temp.size() != 4) continue;
 		if (!testSquare(temp)) continue;
 		if (sortedArea[i] / contourArea(temp) > AREA_RATIO) {
@@ -319,9 +395,9 @@ static void detectMarker(void)
 	getLowRight(ll, hr);
 	//getLowRightCenter(ll, hr);
 
-	drawQuadrilateral(marker[sequence[0]], Scalar(0, 0, 255));
-	drawQuadrilateral(marker[sequence[1]], Scalar(0, 255, 0));
-	drawQuadrilateral(marker[sequence[2]], Scalar(255, 0, 0));
+	//drawQuadrilateral(marker[sequence[0]], Scalar(0, 0, 255));
+	//drawQuadrilateral(marker[sequence[1]], Scalar(0, 255, 0));
+	//drawQuadrilateral(marker[sequence[2]], Scalar(255, 0, 0));
 	//circle(_draw, lowLeft * _scale, 2, Scalar(0, 0, 255), FILLED, LINE_AA);
 	//circle(_draw, highLeft * _scale, 2, Scalar(0, 255, 0), FILLED, LINE_AA);
 	//circle(_draw, highRight * _scale, 2, Scalar(255, 0, 0), FILLED, LINE_AA);
@@ -360,22 +436,24 @@ static void getBarcode2DCenter(Mat &input, Mat &output)
 	warpPerspective(input, output, tran, Size(BAR_2D_WIDTH, BAR_2D_HEIGHT));
 }
 
-static void drawQuadrilateral(vector<Point> quadrilateral, Scalar color)
+static void drawQuadrilateral(vector<Point> &quadrilateral, Scalar color)
 {
-	for (int i = 0; i < 4; i++) {
-		line(_draw, quadrilateral[i] * _scale, quadrilateral[(i + 1) % 4] * _scale, color, 1, LINE_AA);
+	int size = quadrilateral.size();
+	for (int i = 0; i < size; i++) {
+		line(_draw, quadrilateral[i] * _scale, quadrilateral[(i + 1) % size] * _scale, color, 1, LINE_AA);
+		circle(_draw, quadrilateral[i] * _scale, 2, color, FILLED, LINE_AA);
 	}
 }
 
-static void drawConvexHull(int index, vector<int> hull, Scalar color)
+static void drawConvexHull(vector<Point> &hull, Scalar color)
 {
 	Point prev, cur;
 	int size = hull.size();
-	prev = contours[index][hull[size - 1]] * _scale;
+	prev = hull[size - 1] * _scale;
 	for (int i = 0; i < size; i++) {
-		cur = contours[index][hull[i]] * _scale;
+		cur = hull[i] * _scale;
 		line(_draw, prev, cur, color, 1, LINE_AA);
-		circle(_draw, cur, 2, color, FILLED, LINE_AA);
+		//circle(_draw, cur, 2, color, FILLED, LINE_AA);
 		prev = cur;
 	}
 }
